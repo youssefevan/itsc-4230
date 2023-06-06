c70f1361d32cf57820f0ac1770ac367997d16bba
Evan <eyoussef128@gmail.com>
2023-06-05 21:59:44 -0400

    A* pathfinding

--- ITSC4230/Assets/Scripts/Entities/PlayerController.cs
+++ ITSC4230/Assets/Scripts/Entities/PlayerController.cs
@@ -7,18 +7,23 @@ public class PlayerController : Entity
 {
     Vector2 moveInput;
     Rigidbody2D rb;
+    CircleCollider2D vibrationCircle;
     [SerializeField] private float moveSpeed = 50.0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        vibrationCircle = GetComponent<CircleCollider2D>();
     }
 
     private void FixedUpdate() {
         rb.velocity = moveInput.normalized * moveSpeed * Time.fixedDeltaTime;
 
         if (rb.velocity != Vector2.zero && canVibrate == true) {
+            vibrationCircle.enabled = true;
             StartCoroutine(CreateVibrations());
+        } else {
+            vibrationCircle.enabled = false;
         }
 
     }
--- ITSC4230/Assets/Scripts/Entities/SandwormController.cs
+++ ITSC4230/Assets/Scripts/Entities/SandwormController.cs
@@ -1,21 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class SandwormController : Entity
 {
+    private Rigidbody2D rb;
+
+    GameObject target;
+    //Vector2 directionVector;
+    AIDestinationSetter aiDest;
+    
+    //public float moveSpeed;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        aiDest = GetComponent<AIDestinationSetter>();
+    }
+
     private void FixedUpdate()
     {
         if (canVibrate == true) {
             StartCoroutine(CreateVibrations());
         }
+
+        aiDest.target = target.transform;
+
+        /*if (target != null) {
+            directionVector = (target.transform.position - transform.position).normalized;
+            rb.velocity = directionVector * moveSpeed * Time.fixedDeltaTime;
+        } else {
+            rb.velocity = Vector2.zero;
+        }*/
+
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other is CircleCollider2D) {
-            // get game object, check vibration type
+            // get game object, check vibration type, assign target
             if (other.gameObject.GetComponent<Entity>() == true) {
-                Debug.Log(other.gameObject.GetComponent<Entity>().vibrationType);
+                String detectedVibrationType = other.gameObject.GetComponent<Entity>().vibrationType.ToString();
+                if (detectedVibrationType == "Active") {
+                    target = other.gameObject;
+                }
             }
         }
     }
